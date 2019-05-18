2143d3909399938a51d23652f319f4a197358a9d
Syamala Myneni <syamam1009@gmail.com>
2019-05-18 00:21:24 -0500

    Spring boot custom starter

---
+++ README.md
@@ -0,0 +1,9 @@
+### Auto Starter
+- **greeter-library**: The sample library that we're creating the starter for.
+
+- **greeter-spring-boot-autoconfigure**: The project containing the auto-configuration for the library.
+
+- **greeter-spring-boot-starter**: The custom starter for the library.
+
+- **greeter-spring-boot-sample-app**: The sample project that uses the custom starter.
+
---
+++ greeter-spring-boot-sample-app/src/test/java/org/pt/SpringContextIntegrationTest.java
@@ -0,0 +1,17 @@
+package org.pt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.pt.greeter.sample.GreeterSampleApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GreeterSampleApplication.class)
+public class SpringContextIntegrationTest {
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    }
+}
